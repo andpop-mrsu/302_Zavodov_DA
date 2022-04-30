25c6b63f994d051550e0f78a0b0f1e2e1325f869
DZavodov <100023402+DZavodov@users.noreply.github.com>
2022-04-30 15:52:07 +0300

    Task04

---
+++ Task04/src/run_test.php
@@ -0,0 +1,10 @@
+<?php
+namespace Zavodovda\Task04;
+
+#require __DIR__ . '/../vendor/autoload.php';
+require 'StackInterface.php';
+require 'Stack.php';
+require 'checkIfBalanced.php';
+require 'Test.php';
+
+runTest();
